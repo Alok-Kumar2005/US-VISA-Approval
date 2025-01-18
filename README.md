6402537abc493c992db1ecc75c3cb9f04fc93ddd
Alok-Kumar2005 <ay747283@gmail.com>
2025-01-18 23:24:49 +0530

    project setup

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # US-VISA-Approval
+
+```
+conda create -n visa python=3.8 -y
+```
+
+```
+conda activate visa
+```
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup , find_packages
+
+setup(
+    name="us-visa",
+    version="0.0.0",
+    author="Alok Kumar",
+    author_email="ay747283@gmail.com",
+    packages=find_packages()
+)
