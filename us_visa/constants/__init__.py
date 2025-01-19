f49bedfa8def1fe5e66853a9250f4634fe47f4d7
Alok-Kumar2005 <ay747283@gmail.com>
2025-01-19 17:53:42 +0530

    data validation and data drift

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -1,5 +1,5 @@
 import os
-from datetime import datetime
+from datetime import datetime , date
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -17,6 +17,14 @@ MODEL_FILE_NAME = "model.pkl"
 TRAIN_FILE_NAME:str = "train.csv"
 TEST_FILE_NAME: str = "test.csv"
 
+
+TARGET_COLUMN = "case_status"
+CURRENT_YEAR = date.today().year
+PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
+SCHEMA_FILE_PATH = os.path.join("config" , "schema.yaml")
+
+
+
 """
 Data Ingeestion related constant start with DATA_INGESTION var name
 """
@@ -25,3 +33,10 @@ DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_score"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
+
+"""
+Data Validation related constant start with DATA_VALIDATION VAR NAME
+"""
+DATA_VALIDATION_DIR_NAME : str = "data_validation"
+DATA_VALIDATION_DRIFT_REPORT_DIR : str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME:str = "report.yaml"
